2af876d33635efc7d02d3597aafdb87e0b6e8921
zq_prog <zq_prog@163.com>
2019-11-21 14:26:30 +0800

    perfect

--- src/components/topicInfo/replies.js
+++ src/components/topicInfo/replies.js
@@ -1,13 +1,36 @@
 import Taro, { Component } from '@tarojs/taro';
-import { View, Text, Button } from '@tarojs/components';
+import { View, Text, Button, Image } from '@tarojs/components';
 import { connect } from '@tarojs/redux';
+import './replies.scss';
 
 class Replies extends Component {
   render() {
     const { replies } = this.props;
     
     return (
-      <View>Replies</View>
+      <View className='topicInfo-replies'>
+        {replies.map(item => {
+          return (
+            <View 
+              className='topicInfo-reply'
+              key={item.id}
+            >
+              <Image className='topicInfo-reply-image'></Image>
+              <View className='topicInfo-reply-right'>
+                <View className='topicInfo-reply-right-body'>
+                  <View className='topicInfo-reply-right-pie'></View>
+                  <View className='topicInfo-reply-right-content'></View>
+                </View>
+                <View className='topicInfo-reply-right-zan'>
+                  <Image></Image>
+                  <Text></Text>
+                  <Image></Image>
+                </View>
+              </View>
+            </View>
+          )
+        })}
+      </View>
     )
   }
 }
