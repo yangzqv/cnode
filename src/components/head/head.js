ff2e45eefc4498653013c24ba22458afcccce653
yangzq <yangzq_0214@163.com>
2019-12-01 23:01:53 +0800

    h5 端兼容还需处理

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro';
-import { View, Text, Button, Image } from '@tarojs/components';
+import { View, Text, Button, Image } from "@tarojs/components";
 import { connect } from '@tarojs/redux';
 import './head.scss';
 import loginBack from '../../assets/img/loginBack.jpg';
